41cb3ae4a81b652d4b5d0fd185e3ec577226b66d
Gustavo <glinares@unsa.edu.pe>
2024-11-01 10:16:32 -0500

    Version 5 - Modificando codigo perl

--- cgi-bin/Calculadora.pl
+++ cgi-bin/Calculadora.pl
@@ -30,5 +30,40 @@ sub manejar_calculo {
 
     return generar_pagina($expresion, $resultado);
 }
+sub generar_pagina {
+    my ($expresion, $resultado) = @_;
+
+    # Generar encabezado y estructura de la página
+    print $cgi->header('text/html; charset=UTF-8');
+    print "<!DOCTYPE html>";
+    print "<html lang=\"es\">";
+    print "<head>";
+    print "<title>Calculadora</title>";
+    print "<meta charset=\"UTF-8\">";
+    print "<link rel=\"stylesheet\" href=\"../css/style.css\">";
+    print "</head>";
+    print "<body>";
+
+    print "<div class=\"calculator-container\">";
+    print "<h1>Calculadora</h1>";
+    print "<form action=\"/cgi-bin/Calculadora.pl\" method=\"post\">";
+    print "<input type=\"text\" name=\"expresion\" placeholder=\"Ingrese expresión\" value=\"" . $expresion . "\" required>";
+    print "<button type=\"submit\">Calcular</button>";
+    print "</form>";
+
+    if ($resultado) {
+        print "<h2>Resultado: $resultado</h2>";
+    }
+
+    print "<footer>";
+    print "<p>Desarrollado por Gustavo Linares Aquino &copy; 2024 - Programación Web</p>";
+    print "</footer>";
+    print "</div>";
+
+    print "</body>";
+    print "</html>";
+}
+
+manejar_calculo();
 
 
