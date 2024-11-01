8930acfffa99742a89c6b9416ccee6811d8652be
Gustavo <glinares@unsa.edu.pe>
2024-11-01 12:37:19 -0500

    Version10 - Modificando Calculadora.pl

--- cgi-bin/Calculadora.pl
+++ cgi-bin/Calculadora.pl
@@ -6,66 +6,59 @@ use utf8;
 
 my $cgi = CGI->new;
 
+# Imprimir el encabezado de tipo de contenido
 print $cgi->header('text/html; charset=UTF-8');
 
-sub manejar_calculo {
-    my $expresion = $cgi->param('expresion') // '';  
-    my $resultado;
+# Capturar la expresión ingresada
+my $expresion = $cgi->param('expresion') // '';
 
-    if ($expresion) {
-        
-        if ($expresion =~ /^[\d\+\-\*\/\(\) ]+$/) {
-            
-            eval {
-                $resultado = eval $expresion;
-            };
-            
-            if ($@) {
-                $resultado = "Hubo un error en la expresión: $@";
-            }
-        } else {
-            $resultado = "Expresión inválida. Solo se permiten números y los operadores +, -, *, /.";
+# Inicializar variable para el resultado
+my $resultado;
+
+# Verificar si se ingresó una expresión válida y evaluarla
+if ($expresion ne '') {
+    if ($expresion =~ /^[\d\+\-\*\/\(\) ]+$/) {
+        eval {
+            $resultado = eval $expresion;
+        };
+        if ($@) {
+            $resultado = "Hubo un error en la expresión: $@";
         }
     } else {
-        $resultado = "No se ingresó ninguna expresión.";
+        $resultado = "Expresión inválida. Solo se permiten números y los operadores +, -, *, /.";
     }
-
-    return generar_pagina($expresion, $resultado);
-}
-sub generar_pagina {
-    my ($expresion, $resultado) = @_;
-
-    # Generar encabezado y estructura de la página
-    print $cgi->header('text/html; charset=UTF-8');
-    print "<!DOCTYPE html>";
-    print "<html lang=\"es\">";
-    print "<head>";
-    print "<title>Calculadora</title>";
-    print "<meta charset=\"UTF-8\">";
-    print "<link rel=\"stylesheet\" href=\"../css/style.css\">";
-    print "</head>";
-    print "<body>";
-
-    print "<div class=\"calculator-container\">";
-    print "<h1>Calculadora</h1>";
-    print "<form action=\"/cgi-bin/Calculadora.pl\" method=\"post\">";
-    print "<input type=\"text\" name=\"expresion\" placeholder=\"Ingrese expresión\" value=\"" . $expresion . "\" required>";
-    print "<button type=\"submit\">Calcular</button>";
-    print "</form>";
-
-    if ($resultado) {
-        print "<h2>Resultado: $resultado</h2>";
-    }
-
-    print "<footer>";
-    print "<p>Desarrollado por Gustavo Linares Aquino &copy; 2024 - Programación Web</p>";
-    print "</footer>";
-    print "</div>";
-
-    print "</body>";
-    print "</html>";
+} else {
+    $resultado = "Por favor, ingrese una expresión.";
 }
 
-manejar_calculo();
+# Generar la página HTML con el resultado
+print <<HTML;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="calculator-container">
+        <h1>Calculadora</h1>
+        <form action="/cgi-bin/Calculadora.pl" method="post">
+            <input type="text" name="expresion" placeholder="Ingrese expresión" value="$expresion" required>
+            <button type="submit">Calcular</button>
+        </form>
+HTML
 
+# Mostrar el resultado o el mensaje adecuado
+if (defined $resultado) {
+    print "<h2>Resultado: $resultado</h2>";
+}
 
+print <<HTML;
+        <footer>
+            <p>Desarrollado por Gustavo Linares Aquino &copy; 2024 - Programación Web</p>
+        </footer>
+    </div>
+</body>
+</html>
+HTML
