b762fe3453f2a3e5fb7df705fe0cb7742ddf6e55
Gustavo <glinares@unsa.edu.pe>
2024-11-01 19:46:01 -0500

    Version 12 - Modificando codigo perl

--- cgi-bin/Calculadora.pl
+++ cgi-bin/Calculadora.pl
@@ -10,21 +10,19 @@ my $cgi = CGI->new;
 print $cgi->header(-type => "text/html", -charset => "UTF-8");
 
 my $expresion = $cgi->param('expresion') // '';
-my $resultado;
+my $resultado = '';
 
-if (defined $expresion && $expresion ne '') {
+if ($expresion ne '') {
     if ($expresion =~ /^[\d\+\-\*\/\(\) ]+$/) {
         eval {
             $resultado = eval $expresion;
         };
         if ($@) {
-            $resultado = "Hubo un error en la expresión: $@";
+            $resultado = "Expresión incorrecta";
         }
     } else {
-        $resultado = "Expresión inválida. Solo se permiten números y los operadores +, -, *, /.";
+        $resultado = "Expresión incorrecta";
     }
-} else {
-    $resultado = "Por favor, ingrese una expresión.";
 }
 
 print <<HTML;
@@ -40,20 +38,10 @@ print <<HTML;
         <h1>Calculadora</h1>
         <form action="/cgi-bin/Calculadora.pl" method="post">
             <input type="text" name="expresion" placeholder="Ingrese expresión" value="$expresion" required>
+            <input type="text" name="resultado" placeholder="Resultado" value="$resultado" readonly>
             <button type="submit">Calcular</button>
         </form>
-HTML
-
-if (defined $resultado) {
-    print "<h2>Resultado: $resultado</h2>";
-}
-
-print <<HTML;
-        <footer>
-            <p>Desarrollado por Gustavo Linares Aquino &copy; 2024 - Programación Web</p>
-        </footer>
     </div>
 </body>
 </html>
 HTML
-
